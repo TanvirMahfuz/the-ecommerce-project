78f5f3d07a0f02ba12ff87773589996985a9c833
Apurbo <tanvirmahfuz22@gmail.com>
2024-08-09 04:51:21 +0600

    connected mongo

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookie = require("cookie-parser");
+const path = require("path");
+const app = express();
+const storage = require("./static/uploads/upload");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + "/static")));
+app.use(express.urlencoded({extended: true, limit: 10000}));
+app.use(cookie());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+const home = require("./routers/home");
+app.use("/api/home", home);
+
+const cart = require("./routers/cart");
+app.use("/api/cart", cart);
+
+const newItem = require("./routers/newItem");
+app.use("/api/add", newItem);
+
+const search = require("./routers/search");
+app.use("/api/search", search);
+
+app.use("/api/log", require("./routers/auth/login"));
+app.use("/api/sign-up", require("./routers/auth/signUp"));
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,48 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const multer = require("multer");
-const path = require("path");
-const app = express();
-const storage = require("./static/uploads/upload");
-const cookie = require("cookie-parser");
-const upload = multer({storage: storage}).single("file");
-const cors = require("cors");
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
+require("dotenv").config();
+const app = require("./app");
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("database connected");
+    app.listen(3000, () => {
+      console.log("server is running on : http://localhost:3000/api/home");
+    });
   })
-);
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname + "/static")));
-app.use(express.urlencoded({extended: true, limit: 10000}));
-app.use(cookie());
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-const home = require("./routers/home");
-app.use("/api/home", home);
-
-const cart = require("./routers/cart");
-app.use("/api/cart", cart);
-
-const newItem = require("./routers/newItem");
-app.use("/api/add", newItem);
-
-const search = require("./routers/search");
-app.use("/api/search", search);
-
-app.use("/api/log", require("./routers/auth/login"));
-app.use("/api/sign-up", require("./routers/auth/signUp"));
-app.listen(3000, () => {
-  console.log("server is running on : http://localhost:3000/api/home");
-});
+  .catch((err) => {
+    console.log(err);
+  });
